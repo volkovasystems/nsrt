267fe17ba06118674670b961a3222a88c3a30859
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-03 18:38:33 +0800

    Updated test.

--- nsrt-test.js
+++ nsrt-test.js
@@ -1,4 +1,6 @@
-
+const assert = require( "assert" );
 const nsrt = require( "./nsrt.js" );
 
-console.log( nsrt( [ 1, 2, 3, 4 ], 5 ) );
+assert.deepEqual( nsrt( [ 1, 2, 3, 4 ], 5 ), [ 1, 2, 3, 4, 5 ] );
+
+console.log( "ok" );
