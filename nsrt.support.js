91ed694a3d5e21c1afd9ba913c385aaa08b1a0a2
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-09 17:13:46 +0800

    Updated build.

--- nsrt.support.js
+++ nsrt.support.js
@@ -34,6 +34,9 @@
               			"file": "nsrt.js",
               			"module": "nsrt",
               			"author": "Richeve S. Bebedor",
+              			"contributors": [
+              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+              			],
               			"eMail": "richeve.bebedor@gmail.com",
               			"repository": "https://github.com/volkovasystems/nsrt.git",
               			"test": "nsrt-test.js",
