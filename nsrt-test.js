1c57d3747e6b6d6740daee136838f97d00446062
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-28 15:01:03 +0800

    Updated test.

--- nsrt-test.js
+++ nsrt-test.js
@@ -1,8 +1,7 @@
 const assert = require( "assert" );
 const nsrt = require( "./nsrt.js" );
 
-assert.deepEqual( nsrt( [ 1, 2, 3, 4 ], 5 ), [ 1, 2, 3, 4, 5 ], "should be deeply equal" );
-
-assert.deepEqual( nsrt( [ { }, [ ], [ 1, 2 ], 3 ], [ 1, 2 ] ), [ { }, [ ], [ 1, 2 ], 3 ], "should be deeply equal" );
+assert.deepEqual( nsrt( [ 1, 2, 3, 4 ], 5 ),
+	[ 1, 2, 3, 4, 5 ], "should be equal to [ 1, 2, 3, 4, 5 ]" );
 
 console.log( "ok" );
